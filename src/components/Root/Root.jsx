a60e6eb67246ba0f3e4cb5aec5f50992f9014a51
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-03-23 04:04:58 +0200

    Footer added

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 const Root = () => {
   return (
     <>
-      <div className="max-w-5xl mx-auto my-8">
-        <Header></Header>
-        <Outlet></Outlet>
+      <div className="max-w-5xl mx-auto lg:my-8">
+        <Header />
+        <Outlet />
       </div>
+      <Footer />
     </>
   );
 };
